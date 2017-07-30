c06592b547cf2c98070253df37b4bf083b7332a2
takumon <inouetakumon@gmail.com>
2017-07-30 11:51:35 +0900

    １件検索と登録のAPI追加

--- server/routes/article.ts
+++ server/routes/article.ts
@@ -4,7 +4,7 @@ import { Article } from '../models/article';
 
 const articleRouter: Router = Router();
 
-// 全てのメッセージを取得する
+// 全記事を取得する
 articleRouter.get('/', (req, res, next) => {
   Article.find(function(err, doc) {
     if (err) {
@@ -13,12 +13,25 @@ articleRouter.get('/', (req, res, next) => {
           error: err.message
       });
     }
-
-    return res.status(200).json({articles: doc});
+    return res.status(200).json(doc);
   });
 });
 
-// メッセージを登録する
+// 指定したIDの記事を取得する
+articleRouter.get('/:id', (req, res, next) => {
+
+  Article.find({ articleId: +req.params.id }, function(err, doc) {
+    if (err) {
+      return res.status(500).json({
+          title: 'エラーが発生しました。',
+          error: err.message
+      });
+    }
+    return res.status(200).json(doc[0]);
+  });
+});
+
+// 記事を登録する
 articleRouter.post('/', (req, res, next) => {
   const article = new Article({
     title: req.body.title,
@@ -35,6 +48,25 @@ articleRouter.post('/', (req, res, next) => {
 
     return res.status(200).json({
       message: '記事を登録しました。',
+      obj: result
+    });
+  });
+});
+
+articleRouter.put('/:id', (req, res, next) => {
+  Article.update({
+    articleId: req.params.id
+  }, req.body, (err, result) => {
+
+    if (err) {
+      return res.status(500).json({
+          title: 'エラーが発生しました。',
+          error: err.message
+      });
+    }
+
+    return res.status(200).json({
+      message: '記事を更新しました。',
       obj: result
     });
   });
