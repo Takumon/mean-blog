94b28042a8367d76daca5640d1d906595f9a0697
takumon <inouetakumon@gmail.com>
2017-08-12 00:32:18 +0900

    フォーマット微修正

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 
