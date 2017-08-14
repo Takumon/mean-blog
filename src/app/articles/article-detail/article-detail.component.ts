fc5c0e34010f89132a8a5c3cb811ddbf1aa9ba21
takumon <inouetakumon@gmail.com>
2017-08-14 21:44:14 +0900

    記事詳細画面でコメントの追加、削除、更新できるようにした

--- src/app/articles/article-detail/article-detail.component.ts
+++ src/app/articles/article-detail/article-detail.component.ts
@@ -3,8 +3,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import {Location} from '@angular/common';
 
 import { ArticleWithUserModel } from '../shared/article-with-user.model';
+import { CommentModel } from '../shared/comment.model';
 import { ArticleService } from '../shared/article.service';
 import { CurrentUserService } from '../../shared/services/current-user.service';
+import { UserModel } from '../../users/shared/user.model';
 
 
 @Component({
@@ -15,6 +17,8 @@ import { CurrentUserService } from '../../shared/services/current-user.service';
 })
 export class ArticleDetailComponent implements OnInit {
   article: ArticleWithUserModel;
+  newComment: CommentModel;
+
 
   constructor(
     private articleService: ArticleService,
@@ -23,17 +27,32 @@ export class ArticleDetailComponent implements OnInit {
     private location: Location,
     private currentUserService: CurrentUserService,
   ) {
+
   }
 
-
   ngOnInit(): void {
+    this.refreshArticle();
+  }
+
+  user(): UserModel {
+    return this.currentUserService.get().user;
+  }
+
+  refreshArticle(): void {
     this.route.params.subscribe( params => {
       const withUser = true;
       this.articleService.get(+params['id'], withUser)
        .subscribe(article => {
          this.article = article as ArticleWithUserModel;
+         this.refreshComment(article.articleId);
       });
     });
+  }
+
+  refreshComment(articleId: number): void {
+    this.newComment = new CommentModel();
+    this.newComment.user = this.currentUserService.get().user._id;
+    this.newComment.isMarkdown = true;
   }
 
   deleteArticle(): void {
@@ -46,6 +65,36 @@ export class ArticleDetailComponent implements OnInit {
   isMyArticle(): Boolean {
     return this.article.author.userId === this.currentUserService.get().user.userId;
   }
+
+  isMyComment(commentUser): Boolean {
+    return commentUser.userId === this.currentUserService.get().user.userId;
+  }
+
+
+  registerComment(): void {
+    this.articleService
+      .registerComment(this.article.articleId, this.newComment)
+      .subscribe(res => {
+        this.refreshArticle();
+      });
+  }
+
+  updateComment(comment): void {
+    this.articleService
+      .updateComment(this.article.articleId, comment)
+      .subscribe(res => {
+        this.refreshArticle();
+      });
+  }
+
+  deleteComment(commentId: String): void {
+    this.articleService
+      .deleteComment(this.article.articleId, commentId)
+      .subscribe(res => {
+        this.refreshArticle();
+      });
+  }
+
 
   goBack(): void {
     this.location.back();
