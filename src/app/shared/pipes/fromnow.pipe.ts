aa3994d53298151978cd2378abadf2987c24ea2a
takumon <inouetakumon@gmail.com>
2017-09-06 22:02:26 +0900

    fix #9 pipeは作成したが、記事が投稿された日とコメントが追加された日は正確に把握したいので、今回はこの部品は使用しない。　今後必要になる時に備えてpipeだけコミットする

---
+++ src/app/shared/pipes/fromnow.pipe.ts
@@ -0,0 +1,32 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'fromNow'})
+export class FromNowPipe implements PipeTransform {
+  transform(datetime: string, args?) {
+
+    if (!datetime) {
+      return datetime;
+    }
+
+    const date = new Date(datetime);
+    const diff = new Date().getTime() - date.getTime();
+    const d = new Date(diff);
+
+    if (d.getUTCFullYear() - 1970) {
+      return d.getUTCFullYear() - 1970 + '年前';
+    }
+    if (d.getUTCMonth()) {
+      return d.getUTCMonth() + 'ヶ月前';
+    }
+    if (d.getUTCDate() - 1) {
+      return d.getUTCDate() - 1 + '日前';
+    }
+    if (d.getUTCHours()) {
+      return d.getUTCHours() + '時間前';
+    }
+    if (d.getUTCMinutes()) {
+      return d.getUTCMinutes() + '分前';
+    }
+    return d.getUTCSeconds() + '秒前';
+  }
+}
