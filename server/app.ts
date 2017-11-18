ef681c7d2be470d002ed65cf358c49bbe5a31437
takumon <inouetakumon@gmail.com>
2017-11-18 22:51:30 +0900

    #72 アプリ起動時に管理者ユーザが未登録の場合に登録する処理を追加。サーバ側ログ出力はmorganを使うように修正。

--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,8 @@ import * as path from 'path';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 import * as jwt from 'jsonWebToken';
-
+import * as jdenticon from 'jdenticon';
+import * as morgan from 'morgan';
 
 import { MONGO_URL, SECRET } from './config';
 import { authenticateRouter } from './routes/authenticate';
@@ -15,6 +16,11 @@ import { replyRouter } from './routes/reply';
 import { userRouter } from './routes/user';
 import { imageRouter } from './routes/image';
 import { searchConditionRouter } from './routes/search-condition';
+import { ROOT_USER_ID, ROOT_USER_PASSWORD } from './config';
+import { PasswordManager } from './helpers/password-manager';
+import { Image, ImageType } from './models/image';
+
+import { User } from './models/user';
 
 class App {
   public express: express.Application;
@@ -23,9 +29,15 @@ class App {
     this.express = express();
     this.middleware();
     this.routes();
+    this.initDB();
   }
 
   private middleware(): void {
+    if (this.express.get('env') === 'production') {
+      this.express.use(morgan());
+    } else {
+      this.express.use(morgan({ format: 'dev', immediate: false }));
+    }
     this.express.use(bodyParser.json({limit: '50mb'}));
     this.express.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
     mongoose.Promise = global.Promise;
@@ -60,6 +72,53 @@ class App {
     this.express.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'public/index.html'));
     });
+  }
+
+  // アプリ初期化時に管理者権限のユーザを作成する
+  private initDB(): void {
+    User
+      .findOne({ userId : ROOT_USER_ID })
+      .exec((err, user) => {
+        if (err) {
+          // TODO エラー処理
+          console.log('ユーザ検索でエラーが発生しました。', err);
+          return;
+        }
+
+        if (user) {
+          return;
+        }
+
+        const rootUser = new User();
+        rootUser.userId = ROOT_USER_ID;
+        rootUser.password = PasswordManager.crypt(ROOT_USER_PASSWORD);
+        rootUser.isAdmin = true;
+        rootUser.save(err2 => {
+          if (err2) {
+            // TODO エラー処理
+            console.log('管理者ユーザ登録時にエラーが発生しました。', err2);
+            return;
+          }
+
+          // アバター登録
+          const rootAvator = new Image({
+            author: rootUser._id,
+            data: jdenticon.toPng(rootUser.userId, 200),
+            contentType: 'image/png',
+            fileName: `avator_${rootUser.userId}.png`,
+            type: ImageType.AVATOR,
+          });
+
+          rootAvator.save((err3) => {
+            if (err3) {
+              // TODO エラー処理
+              console.log('管理者ユーザのアバター登録時にエラーが発生しました。', err3);
+              return;
+            }
+          });
+        });
+      });
+
   }
 }
 
