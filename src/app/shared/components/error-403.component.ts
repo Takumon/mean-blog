9054afedadab78c2d210e90e111da83ba6774f87
takumon <inouetakumon@gmail.com>
2017-11-19 18:26:27 +0900

    #93 403エラー画面作成（htmlとscssは共通化）

--- src/app/shared/components/error-403.component.ts
+++ src/app/shared/components/error-403.component.ts
@@ -3,11 +3,20 @@ import { OnInit, OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-error-403',
-  templateUrl: './error-403.component.html',
-  styleUrls: ['./error-403.component.scss']
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
 })
 export class Error403Component implements OnInit, OnDestroy {
+  public errorCode: string;
+  public errorMessage: string;
+
+  constructor() {
+
+  }
+
   ngOnInit(): void {
+    this.errorCode = '403';
+    this.errorMessage = '権限エラーです';
     console.log('init');
   }
   ngOnDestroy(): void {
