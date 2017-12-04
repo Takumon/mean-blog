fb4969de7ae20220d7663b7885fcc78efbc08e81
takumon <inouetakumon@gmail.com>
2017-12-05 00:40:37 +0900

    #179 テスト結果をxmlで出力しCircleCIで保存するようにした。テストカバレッッジもCircleCIで保存するようにした

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
-      require('karma-scss-preprocessor')
+      require('karma-scss-preprocessor'),
+      require('karma-junit-reporter'),
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -23,7 +24,7 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -40,6 +41,12 @@ module.exports = function (config) {
     preprocessors: {
       './src/test.ts': ['@angular/cli'],
       './src/styles.scss': ['scss']
+    },
+    junitReporter: {
+       outputDir: 'dist/tests',
+       outputFile: 'test-results.xml',
+       suite: '',
+       useBrowserName: false
     },
   });
 };
