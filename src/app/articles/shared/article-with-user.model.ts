20326b7fdcded28214430d560063a54372759d55
takumon <inouetakumon@gmail.com>
2017-12-04 16:43:29 +0900

    #179 article-detail.serviceのテスト修正（失敗するテストをひとまず成功するように修正）

--- src/app/articles/shared/article-with-user.model.ts
+++ src/app/articles/shared/article-with-user.model.ts
@@ -4,19 +4,35 @@ import { ImageModel } from '../../shared/models/image.model';
 import { CommentWithUserModel } from './comment-with-user.model';
 import { CommentModel } from './comment.model';
 
+/**
+ * 記事(投稿したユーザ情報、いいね情報、コメント情報を含む)のモデル
+ */
 export class ArticleWithUserModel {
+  /** id */
   _id: string;
+  /** 記事タイトル */
   title: string;
+  /** 記事本文 */
   body: string;
+  /** 記事がマークダウン形式か(trueの場合はマークダウン形式、falseの場合はプレーンテキスト形式) */
   isMarkdown: boolean;
+  /** 記事投稿者のユーザ情報 */
   author: UserModel;
-  vote: Array<UserModel>;
-  comments: Array<CommentWithUserModel>;
-  image: Array<ImageModel>;
-  newComment: CommentModel;
+  /** 記事に対するいいねしたユーザのリスト */
+  vote?: Array<UserModel>;
+  /** 記事に対するコメント(コメントしたユーザ情報を含む）のリスト */
+  comments?: Array<CommentWithUserModel>;
+  /** 記事本文で使用している画像情報のリスト */
+  image?: Array<ImageModel>;
+  /** 登録日時 */
   created: string;
+  /** 更新日時 */
   updated: string;
-  deleted: string;
-  // 表示制御用
-  showDetail: boolean;
+  /** （論理）削除日時 */
+  deleted?: string;
+
+  /** （表示制御用）記事に対しての新規コメントのモデル*/
+  newComment?: CommentModel;
+  /** （表示制御用）記事一覧にて記事の詳細(いいねとコメント)を表示するか */
+  showDetail?: boolean;
 }
