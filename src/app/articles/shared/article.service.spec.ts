08cd09c7eafac044783700605d5c76d2fb9b938f
takumon <inouetakumon@gmail.com>
2017-12-17 20:34:48 +0900

    #103 getメソッドのテスト作成

--- src/app/articles/shared/article.service.spec.ts
+++ src/app/articles/shared/article.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, getTestBed, async, inject } from '@angular/core/testing';
 import { HttpRequest } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
@@ -6,9 +6,12 @@ import { JwtService } from '../../shared/services/jwt.service';
 import { LocalStrageService } from '../../shared/services/local-strage.service';
 
 import { ArticleService } from './article.service';
+import { ArticleModel } from './article.model';
+import { ArticleComponent } from '../article-list/article.component';
 
 
 describe('ArticleService', () => {
+  let injector: TestBed;
   let service: ArticleService;
   let httpMock: HttpTestingController;
 
@@ -22,12 +25,59 @@ describe('ArticleService', () => {
       ],
     });
 
-    service = TestBed.get(ArticleService);
-    httpMock = TestBed.get(HttpTestingController);
+    injector = getTestBed();
+    service = injector.get(ArticleService);
+    httpMock = injector.get(HttpTestingController);
   });
 
   it('オブジェクトが生成されるべき', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('get', () => {
+    const mockResponse: any = {
+      count: 1,
+      articles: [
+        {
+          _id: '123456789011',
+          title: '記事タイトル',
+          isMarkdown: false,
+          body: '記事本文',
+          author: '123456789044',
+          created: '20150101 12:34:30',
+          updated: '20150101 12:34:30'
+        }
+      ]
+    };
+
+    const arg_condition = {author: '123456789044'};
+    const arg_pageCondition = {skip: 0, limit: 10};
+    const arg_withUser = true;
+
+    service.get(arg_condition, arg_pageCondition, arg_withUser).subscribe(({count, articles}) => {
+      expect(count).toEqual(1);
+      expect(articles[0]._id).toEqual('123456789011');
+      expect(articles[0].title).toEqual('記事タイトル');
+      expect(articles[0].isMarkdown).toEqual(false);
+      expect(articles[0].body).toEqual('記事本文');
+      expect(articles[0].author).toEqual('123456789044');
+      expect(articles[0].created).toEqual('20150101 12:34:30');
+      expect(articles[0].updated).toEqual('20150101 12:34:30');
+    });
+
+
+    const req = httpMock.expectOne((request: HttpRequest<any>) => {
+      return request.url === '/api/articles';
+    });
+
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.params.get('condition')).toEqual('{"author":"123456789044","skip":0,"limit":10}');
+    expect(req.request.params.get('withUser')).toEqual('true');
+
+    // レスポンス返却
+    req.flush(mockResponse);
+  });
+
 
 });
