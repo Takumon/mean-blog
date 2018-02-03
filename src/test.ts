fe940fd629ddc260cf77e3db290924f4b9ffe31e
takumon <inouetakumon@gmail.com>
2018-02-03 20:40:35 +0900

    #122 tslintの設定変更

--- src/test.ts
+++ src/test.ts
@@ -25,7 +25,8 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /icon-generator\.service\.spec\.ts$/);
+// const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
